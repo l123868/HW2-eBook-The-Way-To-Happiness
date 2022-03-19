9ed63cb22121937ec766be22b483b589b464f9f6
Latifah <l123868@gmail.com>
2022-03-19 21:21:23 +0800

    Initial Commit

---
+++ HW2 eBook-The Way To Happiness/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW2 eBook-The Way To Happiness
+//
+//  Created by Latifah on 2022/3/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
